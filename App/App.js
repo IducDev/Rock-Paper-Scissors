1957c0f4d2062f50b0498d2246cbab0a2fcdabbe
isaac urdaneta <urdanetacuarteisaacdavid@gmail.com>
2022-05-30 04:02:43 -0400

    avance terminado

--- App/App.js
+++ App/App.js
@@ -1,14 +1,11 @@
 import cardsToPlay from "./Componets/CardsToPlay.js";
+import rules from "./Componets/Rules.js";
 import LocalStorage from "./LocalStorage.js";
-
-
 const App=()=>{
     const $root = document.getElementById("root");
     $root.innerHTML = null;
     $root.appendChild(cardsToPlay())
-
-
+    $root.appendChild(rules())
     LocalStorage();
 }
-
 export default App;
---
+++ App/Componets/Rules.js
@@ -0,0 +1,29 @@
+
+
+const Rules =()=>{
+
+    let rules = document.createElement("div");
+
+    // rules.classList.add("rules", "pt-2", "mt-4");
+    // rules.style.display="flex";
+    // rules.style.justifyContent="start";
+
+
+    rules.innerHTML=`
+    
+    <div class="rules pt-2 mt-4" style="display: flex; justify-content: start;">
+        <a class="btn-rules" style="margin: 0 auto;">
+        Rules
+        </a>
+    </div>
+    <span style="color: white; display: block; margin-top: 3rem;">This project is developed with love by <a href="#" style="color: rgb(228, 224, 22);">Isaac Urdaneta</a></span>
+    
+    `;
+
+
+    return rules;
+
+
+}
+
+export default Rules;
